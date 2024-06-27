1131bc7ca057416df7932c3bdea968cc965378be
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2024-06-27 13:06:43 +0545

    feat: encapsulate the function inside a Class for better usability and make publish ready

--- packages/twin-date-converter/src/index.ts
+++ packages/twin-date-converter/src/index.ts
@@ -3,23 +3,75 @@ import { nepEngCalenderMaps } from './nepali-english-calender-maps';
 import { nepDateNoOfDays } from './nepali-month-total-days';
 import { months } from './nepali-english-month-name';
 
-export function getBikramSambatMonthNoOfDays(year: number) {
-  const nepaliYearData = nepDateNoOfDays.find((date) => date.nepYear === year);
-
-  if (!nepaliYearData)
-    throw new Error(
-      `No data available for the year ${year} in the Bikram Sambat calendar.`
-    );
-
-  return months.map((month) => ({
-    monthNumber: month.id,
-    monthName: month.nepName,
-    numberOfDays: nepaliYearData[`m${month.id}` as keyof typeof nepaliYearData],
-  }));
+interface BikramSambatProps {
+  bsYear: number;
+  bsMonth: number;
+  bsDay: number;
+  weekDay: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+  adDate: Date;
+  bsMonthName: string;
 }
 
-function getMonthsWithCumulativeDays(year: number) {
-  return getBikramSambatMonthNoOfDays(year).reduce(
+const _isLeap = (year: number) => {
+  return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+};
+
+const _getADMonthTotalDays = (month: number, year: number) => {
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+    case 2:
+      return _isLeap(year) ? 29 : 28;
+    default:
+      throw new Error(
+        `Invalid date month: ${month}. Month should be between 1 and 12.`
+      );
+  }
+};
+
+const _parseAdString = (date: string) => {
+  if (typeof date === 'string') {
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(date))
+      throw new Error(
+        `Invalid date format: "${date}". Expected format is YYYY-MM-DD.`
+      );
+    const [year, month, day] = date.split('-').map((num) => parseInt(num)) as [
+      number,
+      number,
+      number,
+    ];
+
+    if (month < 1 || month > 12)
+      throw new Error(
+        `Invalid date month: ${month}. Month should be between 1 and 12.`
+      );
+
+    const totalPossibleDays = _getADMonthTotalDays(month, year);
+
+    if (day < 1 || month > totalPossibleDays)
+      throw new Error(
+        `Invalid date day: ${day}. Day should be between 1 and ${totalPossibleDays} for month ${month}.`
+      );
+
+    return new Date(date);
+  }
+
+  throw new Error('Unable to parse AD string.');
+};
+
+const _getMonthsWithCumulativeDays = (year: number) => {
+  return BikramSambat.getBikramSambatMonths(year).reduce(
     (
       accumulatedMonths: {
         monthNumber: number;
@@ -44,110 +96,164 @@ function getMonthsWithCumulativeDays(year: number) {
     },
     []
   );
-}
+};
 
-export function parseEnglishDate(englishDate: Date = new Date()) {
-  const englishDateDayjs = dayjs(englishDate);
-  const matchingCalendarPeriod = nepEngCalenderMaps.find((calendarPeriod) => {
-    const startDate = new Date(calendarPeriod.startDate);
-    const endDate = new Date(calendarPeriod.endDate);
+export default class BikramSambat implements BikramSambatProps {
+  bsYear!: number;
+  bsMonth!: number;
+  bsDay!: number;
+  weekDay!: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+  adDate!: Date;
+  bsMonthName!: string;
 
-    return +endDate >= +englishDate && +startDate <= +englishDate;
-  });
+  private constructor(props: BikramSambatProps) {
+    Object.assign(this, props);
+  }
 
-  if (!matchingCalendarPeriod)
-    throw new Error(
-      `No corresponding Bikram Sambat date found for the English date ${englishDate.toISOString().split('T')[0]}.`
-    );
+  static parse(date: string) {
+    if (!(typeof date === 'string'))
+      throw new Error('This function only accepts string.');
 
-  const daysSinceStartOfPeriod =
-    englishDateDayjs.diff(matchingCalendarPeriod.startDate, 'days') + 1;
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(date))
+      throw new Error(
+        `Invalid date format: "${date}". Expected format is YYYY-MM-DD.`
+      );
 
-  const bsMonthsWithCumulativeDays = getMonthsWithCumulativeDays(
-    matchingCalendarPeriod.nepYear
-  );
-
-  const currentBsMonth = [...bsMonthsWithCumulativeDays].find(
-    (month) => month.cumulativeDays >= daysSinceStartOfPeriod
-  );
-
-  if (!currentBsMonth)
-    throw new Error(
-      `Unable to determine the Bikram Sambat month for the given English date ${englishDate.toISOString().split('T')[0]}.`
-    );
-
-  const previousBsMonth = bsMonthsWithCumulativeDays
-    .filter((month) => month.cumulativeDays < daysSinceStartOfPeriod)
-    .at(-1);
-
-  const bikramSambatDate = {
-    dayOfWeek: englishDateDayjs.day(),
-    bsYear: matchingCalendarPeriod.nepYear,
-    bsMonth: currentBsMonth.monthNumber,
-    bsDay: previousBsMonth
-      ? daysSinceStartOfPeriod - previousBsMonth.cumulativeDays
-      : daysSinceStartOfPeriod,
-    bsMonthName: currentBsMonth.monthName,
-    engDate: englishDate,
-  };
-
-  return bikramSambatDate;
-}
-
-export function parseBikramSambatDate(bikramSambatDateString: string) {
-  if (!/^\d{4}-\d{2}-\d{2}$/.test(bikramSambatDateString))
-    throw new Error(
-      `Invalid date format: "${bikramSambatDateString}". Expected format is YYYY-MM-DD.`
-    );
-  const [bsYear, bsMonth, bsDay]: [number, number, number] =
-    bikramSambatDateString.split('-').map((num) => parseInt(num)) as [
+    const [year, month, day] = date.split('-').map((num) => parseInt(num)) as [
       number,
       number,
       number,
     ];
 
-  if (isNaN(bsYear) || isNaN(bsMonth) || isNaN(bsDay)) {
-    throw new Error(`Invalid date components in: "${bikramSambatDateString}".`);
+    if (year < 1970 || year > 2111)
+      throw new Error('Year should be between 1970 and 2111.');
+
+    if (month < 1 || month > 12)
+      throw new Error(
+        `Invalid date month: ${month}. Month should be between 1 and 12.`
+      );
+
+    const matchingCalendarPeriod = nepEngCalenderMaps.find(
+      (calendarPeriod) => calendarPeriod.nepYear === year
+    );
+
+    if (!matchingCalendarPeriod)
+      throw new Error(
+        `No corresponding data found for the Bikram Sambat year ${year}.`
+      );
+
+    const totalPossibleDays = matchingCalendarPeriod[
+      `m${month}` as keyof typeof matchingCalendarPeriod
+    ] as number;
+
+    if (day < 1 || day > totalPossibleDays)
+      throw new Error(
+        `Invalid date day: ${day}. Day should be between 1 and ${totalPossibleDays} for month ${month}.`
+      );
+
+    const startOfYearDate = new Date(matchingCalendarPeriod.startDate);
+
+    const bsMonthsWithCumulativeDays = _getMonthsWithCumulativeDays(year);
+
+    const currentMonthIndex = month - 1;
+    const daysSinceStartOfYear =
+      (month === 1
+        ? 0
+        : bsMonthsWithCumulativeDays[currentMonthIndex - 1]!.cumulativeDays) +
+      day;
+
+    const englishDate = dayjs(startOfYearDate).add(
+      daysSinceStartOfYear - 1,
+      'days'
+    );
+
+    const bikramSambatDate = {
+      weekDay: englishDate.day(),
+      bsYear: year,
+      bsMonth: month,
+      bsDay: day,
+      bsMonthName: months[currentMonthIndex]!.nepName,
+      adDate: englishDate.toDate(),
+    };
+
+    return new BikramSambat(bikramSambatDate);
   }
 
-  if (bsMonth < 1 || bsMonth > 12)
-    throw new Error(
-      `Invalid month: ${bsMonth}. Month must be between 1 and 12.`
+  static fromAD(date: string | Date) {
+    if (typeof date === 'string') date = _parseAdString(date);
+    else if (!(date instanceof Date))
+      throw new Error('This function only accepts string or Date object.');
+
+    const englishDateDayjs = dayjs(date);
+    const matchingCalendarPeriod = nepEngCalenderMaps.find((calendarPeriod) => {
+      const startDate = new Date(calendarPeriod.startDate);
+      const endDate = new Date(calendarPeriod.endDate);
+
+      return +endDate >= +date && +startDate <= +date;
+    });
+
+    if (!matchingCalendarPeriod)
+      throw new Error(
+        `No corresponding Bikram Sambat date found for the English date ${date.toISOString().split('T')[0]}.`
+      );
+
+    const daysSinceStartOfPeriod =
+      englishDateDayjs.diff(matchingCalendarPeriod.startDate, 'days') + 1;
+
+    const bsMonthsWithCumulativeDays = _getMonthsWithCumulativeDays(
+      matchingCalendarPeriod.nepYear
     );
 
-  const matchingCalendarPeriod = nepEngCalenderMaps.find(
-    (calendarPeriod) => calendarPeriod.nepYear === bsYear
-  );
-
-  if (!matchingCalendarPeriod)
-    throw new Error(
-      `No corresponding data found for the Bikram Sambat year ${bsYear}.`
+    const currentBsMonth = [...bsMonthsWithCumulativeDays].find(
+      (month) => month.cumulativeDays >= daysSinceStartOfPeriod
     );
 
-  const startOfYearDate = new Date(matchingCalendarPeriod.startDate);
+    if (!currentBsMonth)
+      throw new Error(
+        `Unable to determine the Bikram Sambat month for the given English date ${date.toISOString().split('T')[0]}.`
+      );
 
-  const bsMonthsWithCumulativeDays = getMonthsWithCumulativeDays(bsYear);
+    const previousBsMonth = bsMonthsWithCumulativeDays
+      .filter((month) => month.cumulativeDays < daysSinceStartOfPeriod)
+      .at(-1);
 
-  const currentMonthIndex = bsMonth - 1;
-  const daysSinceStartOfYear =
-    (bsMonth === 1
-      ? 0
-      : bsMonthsWithCumulativeDays[currentMonthIndex - 1]!.cumulativeDays) +
-    bsDay;
+    const bikramSambatDate: BikramSambatProps = {
+      weekDay: englishDateDayjs.day(),
+      bsYear: matchingCalendarPeriod.nepYear,
+      bsMonth: currentBsMonth.monthNumber,
+      bsDay: previousBsMonth
+        ? daysSinceStartOfPeriod - previousBsMonth.cumulativeDays
+        : daysSinceStartOfPeriod,
+      bsMonthName: currentBsMonth.monthName,
+      adDate: date,
+    };
 
-  const englishDate = dayjs(startOfYearDate).add(
-    daysSinceStartOfYear - 1,
-    'days'
-  );
+    return new BikramSambat(bikramSambatDate);
+  }
 
-  const bikramSambatDate = {
-    dayOfWeek: englishDate.day(),
-    bsYear,
-    bsMonth,
-    bsDay,
-    bsMonthName: months[currentMonthIndex]!.nepName,
-    engDate: englishDate.toDate(),
-  };
+  static getBikramSambatMonths(year: number) {
+    const nepaliYearData = nepDateNoOfDays.find(
+      (date) => date.nepYear === year
+    );
 
-  return bikramSambatDate;
+    if (!nepaliYearData)
+      throw new Error(
+        `No data available for the year ${year} in the Bikram Sambat calendar.`
+      );
+
+    return months.map((month) => ({
+      monthNumber: month.id,
+      monthName: month.nepName,
+      numberOfDays:
+        nepaliYearData[`m${month.id}` as keyof typeof nepaliYearData],
+    }));
+  }
+
+  toString() {
+    return `${this.bsMonthName} ${this.bsDay} ${this.bsYear}`;
+  }
+
+  static now() {
+    return this.fromAD(new Date());
+  }
 }
--- packages/twin-date-converter/tsup.config.ts
+++ packages/twin-date-converter/tsup.config.ts
@@ -3,9 +3,10 @@ import { Options, defineConfig } from 'tsup';
 export default defineConfig((options: Options) => ({
   entry: ['src/index.ts'],
   clean: true,
-  format: ['esm', 'cjs'],
+  format: ['esm', 'cjs', 'iife'],
   minify: options.watch ? false : 'terser',
   dts: true,
   treeshake: 'recommended',
+  globalName: 'bikramSambat',
   ...options,
 }));
