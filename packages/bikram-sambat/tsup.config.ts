199e5a071b5bf1cdbdd08c378e0c93deb6d3cddb
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2024-06-28 11:40:45 +0545

    chore: add email to author field in package.json and point to tsconfig.json for tsup

--- packages/bikram-sambat/tsup.config.ts
+++ packages/bikram-sambat/tsup.config.ts
@@ -3,6 +3,7 @@ import type { Options } from 'tsup';
 
 export default defineConfig((options: Options) => ({
   entry: ['src/index.ts'],
+  config: 'tsconfig.json',
   clean: true,
   format: ['esm', 'cjs', 'iife'],
   minify: options.watch ? false : 'terser',
