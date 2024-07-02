92e66a97a4b743d1c18c7d07336c6433c3343ccb
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2024-07-02 10:51:27 +0545

    fix: fix major package publish issue

--- packages/bikram-sambat/tsup.config.ts
+++ packages/bikram-sambat/tsup.config.ts
@@ -1,14 +1,64 @@
 import { defineConfig } from 'tsup';
 import type { Options } from 'tsup';
 
-export default defineConfig((options: Options) => ({
-  entry: ['src/index.ts'],
-  config: 'tsconfig.json',
-  clean: true,
-  format: ['esm', 'cjs', 'iife'],
-  minify: options.watch ? false : 'terser',
-  dts: true,
-  treeshake: 'recommended',
-  globalName: 'BikramSambat',
-  ...options,
-}));
+export default defineConfig((options: Options) => [
+  {
+    ...options,
+    entry: ['src/index.ts'],
+    config: 'tsconfig.json',
+    format: ['cjs'],
+    sourcemap: false,
+    dts: true,
+    clean: true,
+  },
+  {
+    ...options,
+    entry: ['src/index.ts'],
+    config: 'tsconfig.json',
+    format: ['esm'],
+    sourcemap: false,
+    dts: false,
+    clean: true,
+    globalName: 'BikramSambat',
+  },
+  {
+    ...options,
+    outDir: 'dist/min',
+    entry: ['src/index.ts'],
+    config: 'tsconfig.json',
+    format: ['cjs'],
+    sourcemap: true,
+    minify: 'terser',
+    outExtension() {
+      return {
+        js: `.min.js`,
+      };
+    },
+    dts: false,
+    clean: true,
+  },
+  {
+    ...options,
+    outDir: 'dist/min',
+    entry: ['src/index.ts'],
+    config: 'tsconfig.json',
+    format: ['esm'],
+    sourcemap: true,
+    minify: 'terser',
+    outExtension({ format }) {
+      switch (format) {
+        case 'esm':
+          return {
+            js: `.min.mjs`,
+          };
+        default:
+          return {
+            js: `.global.min.js`,
+          };
+      }
+    },
+    dts: false,
+    clean: true,
+    globalName: 'BikramSambat',
+  },
+]);
