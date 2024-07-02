55a1f73d762fce3d5c69d9b75cf128f20b894f1d
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2024-07-02 22:52:48 +0545

    build: only build cjs, dts and esm files

--- packages/bikram-sambat/tsup.config.ts
+++ packages/bikram-sambat/tsup.config.ts
@@ -19,46 +19,5 @@ export default defineConfig((options: Options) => [
     sourcemap: false,
     dts: false,
     clean: true,
-    globalName: 'BikramSambat',
-  },
-  {
-    ...options,
-    outDir: 'dist/min',
-    entry: ['src/index.ts'],
-    config: 'tsconfig.json',
-    format: ['cjs'],
-    sourcemap: true,
-    minify: 'terser',
-    outExtension() {
-      return {
-        js: `.min.js`,
-      };
-    },
-    dts: false,
-    clean: true,
-  },
-  {
-    ...options,
-    outDir: 'dist/min',
-    entry: ['src/index.ts'],
-    config: 'tsconfig.json',
-    format: ['esm'],
-    sourcemap: true,
-    minify: 'terser',
-    outExtension({ format }) {
-      switch (format) {
-        case 'esm':
-          return {
-            js: `.min.mjs`,
-          };
-        default:
-          return {
-            js: `.global.min.js`,
-          };
-      }
-    },
-    dts: false,
-    clean: true,
-    globalName: 'BikramSambat',
   },
 ]);
